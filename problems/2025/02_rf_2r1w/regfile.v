5487161189d8864d14f4987a8055e9568e1249fa
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2026-03-03 16:09:30 +0300

    add regfile

---
+++ problems/2025/02_rf_2r1w/regfile.v
@@ -0,0 +1,26 @@
+module reg_file_2r1w (
+    input  wire        clk,
+
+    input  wire [4:0]  i_rd_addr0,
+    output wire [31:0] o_rd_data0,
+
+    input  wire [4:0]  i_rd_addr1,
+    output wire [31:0] o_rd_data1,
+
+    input  wire [4:0]  i_wr_addr,
+    input  wire [31:0] i_wr_data,
+    input  wire        i_wr_en
+);
+
+    reg [31:0] r [31:0];
+
+    assign o_rd_data0 = r[i_rd_addr0];
+    assign o_rd_data1 = r[i_rd_addr1];
+
+    always @(posedge clk) begin
+        if (i_wr_en) begin
+            r[i_wr_addr] <= i_wr_data;
+        end
+    end
+
+endmodule
