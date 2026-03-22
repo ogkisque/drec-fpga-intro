75ca3b4a680bd4d3a082eedcbc2b7982e70e3f36
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2026-03-22 21:08:46 +0300

    add hex display

--- problems/2025/02_lfsr/lfsr.v
+++ problems/2025/02_lfsr/lfsr.v
@@ -1,16 +1,17 @@
 module lfsr (
     input wire clk,
+    input wire rst_n,
     output wire [7:0] o_data
 );
 
-    reg [7:0] data = 8'b00000001;
+    reg [7:0] data;
 
     wire new_bit;
     assign new_bit = data[7] ^ data[5] ^ data[4] ^ data[3];
     assign o_data = data;
 
-    always @(posedge clk) begin
-        data <= {data[6:0], new_bit};
+    always @(posedge clk or negedge rst_n) begin
+        data <= !rst_n ? 8'b00000001 : {data[6:0], new_bit};
     end
 
 endmodule
--- problems/2025/02_lfsr/tb.v
+++ problems/2025/02_lfsr/tb.v
@@ -3,6 +3,7 @@
 module tb_lfsr;
 
 reg clk = 1'b0;
+reg rst_n = 1'b0;
 
 always begin
     #1 clk <= ~clk;
@@ -10,11 +11,16 @@ end
 
 wire [7:0] data;
 
-lfsr lfsr_mod(.clk(clk), .o_data(data));
+lfsr lfsr_mod(.clk(clk), .rst_n(rst_n), .o_data(data));
 
 initial begin
     $dumpvars;      /* Open for dump of signals */
     $display("Test started...");   /* Write to console */
+    #1
+    rst_n = 1'b0;
+    #2
+    rst_n = 1'b1;
+
     #50 $finish;    /* Stop simulation after 50ns */
 end
 
